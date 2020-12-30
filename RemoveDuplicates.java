4343c80ccbd51cc9e3e2015f0efc1d28bde91413
mehul10101 <31861841+mehul10101@users.noreply.github.com>
2020-12-30 17:52:57 +0530

    Create RemoveDuplicates.java

---
+++ RemoveDuplicates.java
@@ -0,0 +1,17 @@
+class RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(removeDuplicates(nums));
+    }
+
+    private static int removeDuplicates(int[] nums) {
+        int i = 0, j, l = nums.length;
+        for (j = 0; j < l; j++) {
+            if (nums[i] != nums[j]) {
+                nums[i + 1] = nums[j];
+                i++;
+            }
+        }
+        return i + 1;
+    }
+}
